1fd4cb0ea10800b9f9171820e6c9402c07aa0f68
Vladislav Afanasjew <afanasjew.vladick@yandex.ru>
2019-12-12 12:05:38 +0300

    подключен redux

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { Provider } from 'react-redux';
 
-AppRegistry.registerComponent(appName, () => App);
+import {name as appName} from './app.json';
+import configureStore from './src/store/configureStore';
+import App from './src/App';
+
+const store = configureStore();
+console.log('>>', store);
+
+
+const root = () => (
+  <Provider store = { store }>
+    <App />
+  </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => root);
